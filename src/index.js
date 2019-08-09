0c7d807de9f6c114ac765d14c00d0f5e7748fd10
Осипов Евгений <eosipov@htc-cs.ru>
2019-08-09 10:41:09 +0400

    Подчистил код, протестировал работоспособность линтеров

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-const styles = require('./index.module.pcss');
+import styles from './index.module.pcss';
 
 const block = document.getElementById('root');
 block.classList.add(styles.root);
 
-function asd (){
-  return 1;
-}
-asd();
 console.log('hello!');
